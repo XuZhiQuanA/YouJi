6751cd7fe4a82fbcd9c17395d66cc2107fd6a90c
XZQ <2901239731@qq.com>
2019-12-03 13:41:40 +0800

    初始化项目

---
+++ YouJi/YouJi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YouJi
+//
+//  Created by dmt312 on 2019/12/3.
+//  Copyright © 2019 XZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ YouJi/YouJi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YouJi
+//
+//  Created by dmt312 on 2019/12/3.
+//  Copyright © 2019 XZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
